dea0cb559a4efa6add544dd36db0645c24ed8e68
smarpit <smarpitbhatt@gmail.com>
2019-03-17 10:22:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getCurrentYear',()=> {
+	return new Date().getFullYear();
+
+});
+
+// hbs.registerHelper('scream',(text)=>{
+// 	return text.toUpperCase();
+// });
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next) =>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log',log+'\n',(err)=>{
+		if(err){console.log("error!")};
+	});
+
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('maintainence.hbs');
+});
+
+app.get('/', (req,res) => {
+
+	res.render('home.hbs',{
+		pageTitle: 'HOMEPAGE',
+		welcomeMessage: 'Heya friend You are welcomed!',
+	});
+
+});
+
+app.get('/about',(req,res) => {
+
+	res.render('about.hbs',{
+		pageTitle: 'About Page',
+	});
+
+});
+
+app.listen(3000,() => {
+	console.log("server is up and running!");
+});
